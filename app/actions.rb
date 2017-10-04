f89b5f8633d44cf1a00f6a5c1c4740bec76a137b
Mark Sullivan <msull291@me.com>
2017-10-04 16:53:15 +0000

    Finished the Ruby Basics

--- app/actions.rb
+++ app/actions.rb
@@ -8,42 +8,42 @@ end
 
 get '/' do
     post_cat = {
-        username = "msull291",
-        avatar_url = "http://vignette1.wikia.nocookie.net/disney/images/f/ff/James-p-sulley-sullivan.png/revision/latest?cb=20131027234614",
-        photo_url = "https://metrouk2.files.wordpress.com/2017/07/187144066.jpg?w=748&h=498&crop=1",
+        username: "msull291",
+        avatar_url: "http://vignette1.wikia.nocookie.net/disney/images/f/ff/James-p-sulley-sullivan.png/revision/latest?cb=20131027234614",
+        photo_url: "https://metrouk2.files.wordpress.com/2017/07/187144066.jpg?w=748&h=498&crop=1",
         humanized_time_ago: humanized_time_ago(15),
-        like_count = 0,
-        comment_count = 1,
-        comments = [{
-            username: "msull291"
+        like_count: 0,
+        comment_count: 1,
+        comments: [{
+            username: "msull291",
             text: "Look how cute!"
-        ]}
+        }]
     }
     
     post_whale = {
-        username = "kirk_whalum",
-        avatar_url = "http://naserca.com/images/kirk_whalum.jpg",
-        photo_url = "http://naserca.com/images/whale.jpg",
+        username: "kirk_whalum",
+        avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
+        photo_url: "http://naserca.com/images/whale.jpg",
         humanized_time_ago: humanized_time_ago(65),
-        like_count = 0,
-        comment_count = 1,
-        comments = [{
-            username: "kirk_whalum"
+        like_count: 0,
+        comment_count: 1,
+        comments: [{
+            username: "kirk_whalum",
             text: "#weekendvibes"
-        ]}
+        }]
     }
     
     post_marlin = {
-        username = "marlin_peppa",
-        avatar_url = "http://naserca.com/images/marlin_peppa.jpg",
-        photo_url = "http://naserca.com/images/marlin.jpg",
+        username: "marlin_peppa",
+        avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
+        photo_url: "http://naserca.com/images/marlin.jpg",
         humanized_time_ago: humanized_time_ago(190),
-        like_count = 0,
-        comment_count = 1,
-        comments = [{
-            username: "marlin_peppa"
+        like_count: 0,
+        comment_count: 1,
+        comments: [{
+            username: "marlin_peppa",
             text: "lunchtime! ;)"
-        ]}
+        }]
     }
     [post_cat, post_whale, post_marlin].to_s
 end
